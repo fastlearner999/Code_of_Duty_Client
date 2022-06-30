b8d0207a1fbd8b8a3e6604e2f0b4c2209c20ca86
wingyuen2022 <wingyuenuk330@gmail.com>
2022-06-30 16:39:44 +0100

    Added workout related js files

---
+++ js/workout_update.js
@@ -0,0 +1,8 @@
+// ThiS JS FILE IS DESIGNED FOR:
+// - view_workout.html
+
+const API_URL_BASE = 'https://energize-code-of-duty.herokuapp.com';
+const curPage = window.location.pathname;
+if (curPage.includes('view_workout.html')) {
+
+}
