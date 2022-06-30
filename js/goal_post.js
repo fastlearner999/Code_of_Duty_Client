67c514aedab2487a16a401ac30fde926bb3968e8
Aaron Smith <2021manifest@gmail.com>
2022-06-30 17:35:14 +0100

    conflicts resolved

--- js/goal_post.js
+++ js/goal_post.js
@@ -3,16 +3,17 @@ const API_URL_BASE = 'https://energize-code-of-duty.herokuapp.com';
 const addNewGoal = async (e) => {
   e.preventDefault()
   let goalData = {
+        user_id: 5,
         goal_name: document.querySelector('#goal_name').value,
         sport_type: document.querySelector('#sport_type').value,
-        period: document.querySelector('#period').value,
+        period: parseInt( document.querySelector('#period').value),
         period_type: document.querySelector('#period_type').value,
-        start_date: document.querySelector('#start_date').value,
-        end_date: document.querySelector('#end_date').value,
-        target_distance: document.querySelector('#target_distance').value,
+        start_date: null,
+        end_date: null,
+        target_distance: parseInt( document.querySelector('#target_distance').value),
         target_distance_unit: document.querySelector('#target_distance_unit').value
   }
-  const res = await fetch(`API_URL_BASE/goal`, {		
+  const res = await fetch(`${API_URL_BASE}/goal`, {		
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -22,9 +23,9 @@ const addNewGoal = async (e) => {
   });
   const resBody = await res.json();
   console.log(resBody)
-  if (resBody !== undefined && 'id' in resBody) {
-    // window.location.href = 'goals.html';
-    }
+  // if (resBody !== undefined && 'id' in resBody) {
+  //   // window.location.href = 'goals.html';
+  //   }
   };
 
   document.addEventListener("submit", addNewGoal);
