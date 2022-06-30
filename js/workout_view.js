78f7ac837d54f2f9e1730ec82a55e13c62cfb1cb
wingyuen2022 <wingyuenuk330@gmail.com>
2022-06-30 18:17:24 +0100

    Modified view workout

--- js/workout_view.js
+++ js/workout_view.js
@@ -7,7 +7,40 @@ const curPage = window.location.pathname;
 
 
 if (curPage.includes('workouts.html')) {
-
+    async function getAllWorkouts() {
+        try{
+            let url = `${API_URL_BASE}/workout`;
+            const response = await fetch(url)
+            const data = await response.json();
+            data.forEach(workout => {
+                const listTag = document.querySelector('#workout_list');
+    
+                // creating goals card
+                const div = document.createElement('div')
+                const title = document.createElement('h4')
+                title.textContent = workout.sport_type;
+                
+                // styling created elements
+                div.classList = 'goal-div'
+                title.classList = 'goal-title'
+                
+                
+                div.appendChild(title);
+                
+                listTag.appendChild(div);
+                
+                div.addEventListener('click', ()=>{
+                    window.location.replace(`./view_workout.html?id=${workout.id}`);
+                })
+            });
+    
+    
+        }catch(err){
+            return({message: err.message})
+        }
+    }
+    
+    getAllWorkouts();
 }
 
 if (curPage.includes('view_workout.html')) {
