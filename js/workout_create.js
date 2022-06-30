15e776bdc13bc9482c58a9dec54c36e911764897
wingyuen2022 <wingyuenuk330@gmail.com>
2022-06-30 21:10:37 +0100

    Amended the whole workout flow

--- js/workout_create.js
+++ js/workout_create.js
@@ -4,5 +4,11 @@
 const API_URL_BASE = 'https://energize-code-of-duty.herokuapp.com';
 const curPage = window.location.pathname;
 if (curPage.includes('create_workout.html')) {
-
+    
+    const saveWorkoutButton = document.querySelector('#saveWorkoutButton');
+    saveWorkoutButton.addEventListener('click', ()=>{
+        //TODO
+        alert('Saved');
+        window.location.replace(`./workouts.html`);
+    });
 }
--- js/workout_update.js
+++ js/workout_update.js
@@ -4,6 +4,7 @@
 const API_URL_BASE = 'https://energize-code-of-duty.herokuapp.com';
 const curPage = window.location.pathname;
 if (curPage.includes('edit_workout.html')) {
+
     const deleteWorkoutButton = document.querySelector('#deleteWorkoutButton');
     deleteWorkoutButton.addEventListener('click', ()=>{
         //TODO
@@ -11,4 +12,10 @@ if (curPage.includes('edit_workout.html')) {
         window.location.replace(`./workouts.html`);
     });
     
+    const saveWorkoutButton = document.querySelector('#saveWorkoutButton');
+    saveWorkoutButton.addEventListener('click', ()=>{
+        //TODO
+        alert('Saved');
+        window.location.replace(`./workouts.html`);
+    });
 }
