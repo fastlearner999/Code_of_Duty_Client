3441703186a4cde1c9458935aa4e68e8e3bcbc55
wingyuen2022 <wingyuenuk330@gmail.com>
2022-06-30 12:42:15 +0100

    Updated /js/user.js + create_profile + edit_profile

--- js/user.js
+++ js/user.js
@@ -34,13 +34,13 @@ if (curPage.includes('create_profile.html')) {
 	
 		let password1 = document.querySelector("#password1").value;
 		let password2 = document.querySelector("#password2").value;
-		if (password1 !== password2) {
-			alert('Your password is not match.');
+		if (password1.length < 8) {
+			alert('Your password is too short.');
 			return;
 		}
 
-		if (password1.length < 8) {
-			alert('Your password is too short.');
+		if (password1 !== password2) {
+			alert('Your password is not match.');
 			return;
 		}
 	
@@ -50,7 +50,7 @@ if (curPage.includes('create_profile.html')) {
 			return;
 		}
 		
-		let user = {
+		let newUser = {
 				email: email,
 				password: password1,
 				first_name: first_name,
@@ -63,7 +63,7 @@ if (curPage.includes('create_profile.html')) {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify(user)
+			body: JSON.stringify(newUser)
 		});
 		const resBody = await res.json();
 		if (resBody !== undefined && 'id' in resBody) {
@@ -77,6 +77,74 @@ if (curPage.includes('login.html')) {
 	let userLoginForm = document.querySelector("#login-form");
 	userLoginForm.addEventListener('submit', async (e)=>{
 		e.preventDefault();
+
+	});
+}
+
+
+if (curPage.includes('edit_profile.html')) {
+	let userEditForm = document.querySelector("#edit-profile-form");
+	userEditForm.addEventListener('submit', async (e)=>{
+		e.preventDefault();
+		
+		let first_name = document.querySelector("#firstname").value;
+		if (first_name.length < 2) {
+			alert('Your first name is too short.');
+			return;
+		}
+
+		let last_name = document.querySelector("#lastname").value;
+		if (last_name.length < 2) {
+			alert('Your last name is too short.');
+			return;
+		}
+
+		let email = document.querySelector("#email").value;
+		if (!email.toLowerCase()
+		.match(
+		  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		)) {
+			alert('Please input a valid email format.');
+			return;
+		}
+
+		if (email.length < 9) {
+			alert('Your email is too short.');
+			return;
+		}
+	
+		let password1 = document.querySelector("#password1").value;
+		let password2 = document.querySelector("#password2").value;
+		if (password1.length < 8) {
+			alert('Your password is too short.');
+			return;
+		}
+
+		if (password1 !== password2) {
+			alert('Your password is not match.');
+			return;
+		}
+
+		let existUser = {
+			id: document.querySelector("#id").value,
+			email: email,
+			password: password1,
+			first_name: first_name,
+			last_name: last_name,
+			gender: document.querySelector("#gender").value
+		};
+		const res = await fetch(API_URL_BASE + '/user', {		
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(existUser)
+		});
+		const resBody = await res.json();
+		if (resBody !== undefined && 'id' in resBody) {
+			window.location.href = 'goals.html';
+		}
 	});
 }
 
