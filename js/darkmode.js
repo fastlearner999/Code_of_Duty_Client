9896337181af61a7c48a9d1f0fba5a09f3633b6a
Aaron Smith <2021manifest@gmail.com>
2022-07-01 10:35:09 +0100

    removed darkmode experiment , created a login.css

--- js/darkmode.js
+++
@@ -1,6 +0,0 @@
-const btn_Dark_Mode = document.getElementById('btn-dark-mode')
-console.log(btn_Dark_Mode)
-btn_Dark_Mode.addEventListener('click', ()=>{
-    const element = document.body;
-    element.classList.toggle("dark-mode");
-})
